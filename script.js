8494ee332c3ebbf12a235e934de4796dfb99b15e
Cory Bee <cbinder705@gmail.com>
2022-09-18 15:40:39 -0400

    started js..getting stuck

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 // initial commit
+var today = moment().format("ddd, MMM Do YYYY");
+
+$("#currentDay").html(today);
+
+const saveButton = $(".saveBtn");
+
+// block colors
+function tBlockColors() {}
